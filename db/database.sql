6142b51a80872cde8ecb474d09dee1ad9ba25c2f
System Administrator <root@Fabios-MacBook-Air.local>
2024-04-29 09:20:35 -0300

    first commit

---
+++ db/database.sql
@@ -0,0 +1,22 @@
+
+
+
+\c harrypotter;
+
+CREATE TABLE bruxos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    idade INTEGER NOT NULL,
+    casa VARCHAR(50),
+    habilidade VARCHAR(100) NOT NULL,
+    status_sangue VARCHAR(50) NOT NULL,
+    patrono VARCHAR(100)
+);
+
+CREATE TABLE varinhas (
+    id SERIAL PRIMARY KEY,
+    material VARCHAR(100) NOT NULL,
+    comprimento DECIMAL NOT NULL,
+    nucleo VARCHAR(100) NOT NULL,
+    data_fabricacao DATE NOT NULL
+);
